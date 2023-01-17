b20c542bf4e328320d1c4101888bbf9d4c7c62c3
Jorge Fernandez-de-Cossio-Diaz <cossio@users.noreply.github.com>
2023-01-17 14:55:31 +0100

    cd

---
+++ README.md
@@ -0,0 +1,16 @@
+# ContrastiveDivergenceRBM Julia package
+
+[![License](https://img.shields.io/badge/license-MIT-green.svg)](https://github.com/cossio/ContrastiveDivergenceRBM.jl/blob/master/LICENSE.md)
+![](https://github.com/cossio/ContrastiveDivergenceRBM.jl/workflows/CI/badge.svg)
+[![codecov](https://codecov.io/gh/cossio/ContrastiveDivergenceRBM.jl/branch/master/graph/badge.svg?token=O5P8LQTVF3)](https://codecov.io/gh/cossio/ContrastiveDivergenceRBM.jl)
+
+Contrastive divergence (CD) training of RBMs, in Julia.
+
+## Installation
+
+This package is registered. Install with:
+
+```julia
+import Pkg
+Pkg.add("ContrastiveDivergenceRBM")
+```
---
+++ src/ContrastiveDivergenceRBM.jl
@@ -0,0 +1,71 @@
+module ContrastiveDivergenceRBM
+
+using Optimisers: AbstractRule, setup, update!, Adam
+using RestrictedBoltzmannMachines: RBM, moments_from_samples, sample_from_inputs,
+    zerosum!, rescale_weights!, infinite_minibatches, sample_v_from_v,
+    ∂free_energy, ∂regularize!
+
+"""
+    cd!(rbm, data)
+
+Trains the RBM on data using Contrastive divergence.
+"""
+function cd!(
+    rbm::RBM,
+    data::AbstractArray;
+    batchsize::Int = 1,
+    iters::Int = 1, # number of gradient updates
+    steps::Int = 1, # MC steps to update fantasy chains
+    optim::AbstractRule = Adam(), # optimizer rule
+    moments = moments_from_samples(rbm.visible, data), # sufficient statistics for visible layer
+
+    # regularization
+    l2_fields::Real = 0, # visible fields L2 regularization
+    l1_weights::Real = 0, # weights L1 regularization
+    l2_weights::Real = 0, # weights L2 regularization
+    l2l1_weights::Real = 0, # weights L2/L1 regularization
+
+    # gauge
+    zerosum::Bool = true, # zerosum gauge for Potts layers
+    rescale::Bool = true, # normalize weights to unit norm (for continuous hidden units only)
+
+    callback = Returns(nothing), # called for every batch
+
+    shuffle::Bool = true,
+
+    # parameters to optimize
+    ps = (; visible = rbm.visible.par, hidden = rbm.hidden.par, w = rbm.w),
+    state = setup(optim, ps) # initialize optimiser state
+)
+    @assert size(data) == (size(rbm.visible)..., size(data)[end])
+
+    # gauge constraints
+    zerosum && zerosum!(rbm)
+    rescale && rescale_weights!(rbm)
+
+    for (iter, (vd,)) in zip(1:iters, infinite_minibatches(data; batchsize, shuffle))
+        # fantasy chains, sampled from the data
+        vm = sample_v_from_v(rbm, vd; steps)
+
+        # compute gradient
+        ∂d = ∂free_energy(rbm, vd; moments)
+        ∂m = ∂free_energy(rbm, vm)
+        ∂ = ∂d - ∂m
+
+        # weight decay
+        ∂regularize!(∂, rbm; l2_fields, l1_weights, l2_weights, l2l1_weights, zerosum)
+
+        # feed gradient to Optimiser rule
+        gs = (; visible = ∂.visible, hidden = ∂.hidden, w = ∂.w)
+        state, ps = update!(state, ps, gs)
+
+        # reset gauge
+        rescale && rescale_weights!(rbm)
+        zerosum && zerosum!(rbm)
+
+        callback(; rbm, optim, iter, vm, vd)
+    end
+    return state, ps
+end
+
+end # module
